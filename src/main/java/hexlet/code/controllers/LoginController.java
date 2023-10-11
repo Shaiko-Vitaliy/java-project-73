8c65cfe559e50c6b9397bdc23ab19fe3320f1b69
Shaiko-Vitaliy <shaiko2586@gmail.com>
2023-10-11 21:58:53 +0300

    edit LoginControl.java

--- src/main/java/hexlet/code/controllers/LoginController.java
+++ src/main/java/hexlet/code/controllers/LoginController.java
@@ -2,11 +2,13 @@ package hexlet.code.controllers;
 
 import hexlet.code.component.JWTHelper;
 import hexlet.code.dto.LoginDto;
-import hexlet.code.model.User;
-import hexlet.code.service.UserService;
+import hexlet.code.service.UserDetailsServiceImpl;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,17 +23,23 @@ import static org.springframework.security.web.authentication.UsernamePasswordAu
 @RequestMapping("${base-url}")
 public class LoginController {
 
-    private final PasswordEncoder bCryptPasswordEncoder;
     private final JWTHelper jwtHelper;
-    private final UserService userService;
+    private final AuthenticationManager authenticationManager;
+    private final UserDetailsServiceImpl userDetailsService;
 
     @PostMapping(path = "/login")
-    public String logIn(@RequestBody LoginDto dto) {
-        User existedUser = userService.findUserByEmail(dto.getEmail());
-        String passwordToCheck = dto.getPassword();
-        if (!bCryptPasswordEncoder.matches(passwordToCheck, existedUser.getPassword())) {
-            throw new UsernameNotFoundException("Sign in failed. Incorrect password!");
+    public ResponseEntity<String> logIn(@RequestBody LoginDto dto) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        dto.getEmail(),
+                        dto.getPassword())
+        );
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(dto.getEmail());
+        if (userDetails == null) {
+            return ResponseEntity
+                    .status(HttpServletResponse.SC_BAD_REQUEST)
+                    .body("User with username " + dto.getEmail() + " not found");
         }
-        return jwtHelper.expiring(Map.of(SPRING_SECURITY_FORM_USERNAME_KEY, existedUser));
+        return ResponseEntity.ok(jwtHelper.expiring(Map.of(SPRING_SECURITY_FORM_USERNAME_KEY, userDetails)));
     }
 }
